739394ea59e7d6599c0d08acabb8572568efa545
renjij <jrj.lamonf@gmail.com>
2024-08-05 06:45:05 -0400

    add emotion_detector.py

---
+++ emotion_detection.py
@@ -0,0 +1,35 @@
+import requests  # Import the requests library to handle HTTP requests
+import json
+
+def emotion_detector(text_to_analyse):  # Define a function named sentiment_analyzer that takes a string input (text_to_analyse)
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    response = requests.post(url, json = myobj, headers=header)  # Send a POST request to the API with the text and headers
+    formatted_response = json.loads(response.text)
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    anger_score = emotions['anger']
+    disgust_score = emotions['disgust']
+    fear_score = emotions['fear']
+    joy_score = emotions['joy']
+    sadness_score = emotions['sadness']
+
+    emotion_scores = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score
+        }
+    highest_emotion = max(emotion_scores, key=emotion_scores.get)
+
+    result = {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': highest_emotion
+        }
+    return result
